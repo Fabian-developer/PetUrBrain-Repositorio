605d1f5b6e6dc18d887e074125223f1c368d7d8c
Leonardo Renaut <xxforagidoxx@gmail.com>
2021-06-05 10:24:43 -0300

    :sparkles: integration

--- appbrain/lib/main.dart
+++ appbrain/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:flutter/material.dart' hide Router;
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:localize_and_translate/localize_and_translate.dart';
 import 'package:one_context/one_context.dart';
 
@@ -26,6 +27,8 @@ void main() async {
   );
 
   await Firebase.initializeApp();
+
+  MobileAds.instance.initialize();
 
   runApp(Phoenix(child: MyApp()));
 }
