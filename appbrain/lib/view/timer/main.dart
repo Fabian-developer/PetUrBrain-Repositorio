5f10e3cb04106f924540d6f2b444825fffad6da9
Leonardo Renaut <leornt@protonmail.com>
2021-07-14 22:57:15 -0300

    :sparkles: timer controller + animated icon + border

--- appbrain/lib/view/timer/main.dart
+++ appbrain/lib/view/timer/main.dart
@@ -16,11 +16,20 @@ class Timer extends StatefulWidget {
   _TimerState createState() => _TimerState();
 }
 
-class _TimerState extends State<Timer> {
+class _TimerState extends State<Timer> with SingleTickerProviderStateMixin {
+  AnimationController controller;
+
+  final CountDownController countController = CountDownController();
+
   final player = AudioPlayer();
 
   @override
   void initState() {
+    controller = AnimationController(
+      duration: Duration(milliseconds: 250),
+      vsync: this,
+      value: 1,
+    );
     super.initState();
     player.setUrl(RAIN_URL);
   }
@@ -42,43 +51,85 @@ class _TimerState extends State<Timer> {
         children: [
           Expanded(
             child: Center(
-              child: CircularCountDownTimer(
-                autoStart: true,
-                duration: TIMER_DURATION,
-                fillColor: Theme.of(context).colorScheme.primaryVariant,
-                height: MediaQuery.of(context).size.width * .7,
-                isReverse: true,
-                isReverseAnimation: false,
-                isTimerTextShown: true,
-                ringColor: Theme.of(context).colorScheme.primary,
-                strokeCap: StrokeCap.round,
-                strokeWidth: MediaQuery.of(context).size.width * .1,
-                textStyle: TextStyle(fontSize: 32, fontWeight: FontWeight.w800),
-                width: MediaQuery.of(context).size.width * .7,
-                onComplete: () {
-                  Navigator.pop(context);
-                  blocHome.addPoints();
-                },
+              child: Stack(
+                children: [
+                  CircularCountDownTimer(
+                    autoStart: true,
+                    controller: countController,
+                    duration: TIMER_DURATION,
+                    fillColor: Theme.of(context).colorScheme.primaryVariant,
+                    height: MediaQuery.of(context).size.width * .7,
+                    isReverse: true,
+                    isReverseAnimation: false,
+                    isTimerTextShown: true,
+                    ringColor: Theme.of(context).colorScheme.primary,
+                    strokeCap: StrokeCap.round,
+                    strokeWidth: MediaQuery.of(context).size.width * .1,
+                    textStyle:
+                        TextStyle(fontSize: 32, fontWeight: FontWeight.w800),
+                    width: MediaQuery.of(context).size.width * .7,
+                    onComplete: () {
+                      Navigator.pop(context);
+                      blocHome.addPoints();
+                    },
+                  ),
+                  Positioned(
+                    bottom: MediaQuery.of(context).size.width * .12,
+                    left: 0,
+                    right: 0,
+                    child: Center(
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(50),
+                        onTap: () {
+                          if (controller.value == 1) {
+                            countController.pause();
+                            controller.reverse();
+                          } else {
+                            countController.resume();
+                            controller.forward();
+                          }
+                        },
+                        child: AnimatedIcon(
+                          color: Theme.of(context).colorScheme.primaryVariant,
+                          icon: AnimatedIcons.play_pause,
+                          progress: controller,
+                          size: 48,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
-          Container(
-            height: 50,
-            child: TextButton(
+          const SizedBox(height: 10),
+          InkWell(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Theme.of(context).colorScheme.primaryVariant,
+                ),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              height: 50,
+              width: MediaQuery.of(context).size.width * .5,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
                     translator.translate('timer02'),
-                    style: TextStyle(color: Colors.purple),
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primaryVariant,
+                    ),
                   ),
                 ],
               ),
-              onPressed: () {
-                player.playing ? player.pause() : player.play();
-              },
             ),
+            onTap: () {
+              player.playing ? player.pause() : player.play();
+            },
           ),
+          const SizedBox(height: 10),
           Ad(),
         ],
       ),
